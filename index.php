28bd0ed119ebec4aa93a78e2d4543a1e1389fbe7
Oliboy50 <oliver.thebault@gmail.com>
2015-06-22 15:03:12 +0200

    demo php LV

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$lv = "Link Value";
+
+?>
+
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Hello <?= $lv ?></title>
+</head>
+<body>
+
+<p>
+    Hello <?= $lv ?>!
+    I'm running in a container!
+</p>
+
+</body>
+</html>
